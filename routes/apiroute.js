3f2bb5072108f9f310e8a959e9115592b8203b2e
Emily Resch <eresch527@gmail.com>
2020-04-26 00:46:11 -0500

    trying another method

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -14,57 +14,66 @@ module.exports = function (app) {
     //CREATE, SAVE, SEND
     app.post("/api/notes", function (req, res) {
 
-        console.log(req);
+        // console.log(req);
         
         //create random ID number for a new note
-        var noteId = Math.floor(Math.floor(Math.random() * 10));
+        // var noteId = Math.floor(Math.floor(Math.random() * 10));
         //create newNote object setup so there's an ID key/value pair
-        var newNote = {
-            // title: req.body.title,
-            // text: req.body.text,
-            id: noteId
-        };
-
+        // var newNote = {
+        //     // title: req.body.title,
+        //     // text: req.body.text,
+        //     id: noteId
+        // };
         // read the JSON file, parse the data
-        fs.readFile('../db/db.json', "utf8", function (err, res) {
-            if (err) throw err;
-            var everyNote = JSON.parse(res);
+        var savedNotes = JSON.parse(fs.readFile('../db/db.json', function(err){
+                if (err) throw err;
+        }));
+            
+        var newNote = req.body;
+        var noteID = (savedNotes.length).toString(); //get placement of element in array
+        newNote.id = noteID;
             //push user's new note into the JSON object
-            everyNote.push(newNote);
+        savedNotes.push(newNote);
             //stringify user's new note and append it to the JSON file
-            fs.appendFile('../db/db.json', JSON.stringify(everyNote), function (err) {
+        fs.writeFileSync('../db/db.json', JSON.stringify(savedNotes), function (err) {
                 if (err) throw err;
                 //send/show user the note data
-                res.send(noteData);
+                res.json(savedNotes);
                 //test that new note is created
                 console.log("new note created");
             });
 
         });
-    });
+    // });
 
 
     // DELETE notes
     app.delete("/api/notes/:id", function (req, res) {
-
-        //create variable to hold request parameters (specifically note's id)
-        var noteId = req.params.id;
-
-        //read db JSON file and parse
-        fs.readFile('../db/db.json', "utf8", function (err, res) {
+        var savedNotes = JSON.parse(fs.readFile('../db/db.json', function(err){
             if (err) throw err;
-            var everyNote = JSON.parse(res);
-            //use filter to create a new array that includes all notes but the one with the matching ID
-            var newEveryNote = everyNote.filter(note => note.id != noteId);
-            fs.writeFile("./db/db.json", JSON.stringify(newEveryNote, null, 2), err => {
+        }));
+        //create variable to hold request parameters (specifically note's id)
+        var noteID = req.params.id;
+        var newIDs = 0; //start 
+        var newSavedNotes = savedNotes.filter(note => note.id != noteId);
+        //read db JSON file and parse
+
+        for(note of newSavedNotes){
+            note.id = newIDs.toString();
+            newIDs++;
+        }
+
+       
+        fs.writeFileSync("./db/db.json", JSON.stringify(newSavedNotes), function(err) {
                 if (err) throw err;
                 //send/show user the updated note data
-                res.send(noteData);
-                console.log("deleted note")
+                res.json(newSavedNotes);
+                console.log("deleted note with ID " + noteID);
             });
 
             // res.send(noteData);
         });
-    });
+    
 
 }
+
