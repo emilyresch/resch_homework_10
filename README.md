6bf97a92262db3aa5a10a7b6f03765c76b861afd
Emily Resch <eresch527@gmail.com>
2020-04-26 00:14:24 -0500

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # resch_homework_10
 Note Taker
+
+
+This is currently not working. The same errors keep occurring when I mess with it, something to this effect:
+
+
+[Error: ENOENT: no such file or directory, open '../db/db.json'] {
+  errno: -2,
+  code: 'ENOENT',
+  syscall: 'open',
+  path: '../db/db.json'
+}
+
+Need the chance to go over this with someone!! Might be doing too much w the fs.read/write etc... 
--- routes/apiroute.js
+++ routes/apiroute.js
@@ -1,37 +1,42 @@
+//requires/dependencies
 var fs = require("fs");
-// var path = require("path");
 var noteData = require("../db/db.json");
-// var noteDataParse = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-// console.log(noteData);
 
+//GET POST and DELETE METHODS
 module.exports = function (app) {
 
     //READ
     app.get("/api/notes", function (req, res) {
-        // res.sendFile(path.join(__dirname, "..public/notes.html"));
+        //show db.json
         res.send(noteData);
     });
 
-    //SAVE
+    //CREATE, SAVE, SEND
     app.post("/api/notes", function (req, res) {
 
-        // console.log(req);
-        //receive new note to save on the request body
-        // var newNote = req.body;
-        var noteId = Math.floor(Math.floor(Math.random() * 10))
+        console.log(req);
+        
+        //create random ID number for a new note
+        var noteId = Math.floor(Math.floor(Math.random() * 10));
+        //create newNote object setup so there's an ID key/value pair
         var newNote = {
-            title: req.body.title,
-            text: req.body.text,
+            // title: req.body.title,
+            // text: req.body.text,
             id: noteId
         };
 
+        // read the JSON file, parse the data
         fs.readFile('../db/db.json', "utf8", function (err, res) {
             if (err) throw err;
             var everyNote = JSON.parse(res);
+            //push user's new note into the JSON object
             everyNote.push(newNote);
-            fs.writeFile('../db/db.json', JSON.stringify(everyNote), function (err) {
+            //stringify user's new note and append it to the JSON file
+            fs.appendFile('../db/db.json', JSON.stringify(everyNote), function (err) {
                 if (err) throw err;
+                //send/show user the note data
                 res.send(noteData);
+                //test that new note is created
                 console.log("new note created");
             });
 
@@ -39,37 +44,27 @@ module.exports = function (app) {
     });
 
 
-    // DELETE
-
+    // DELETE notes
     app.delete("/api/notes/:id", function (req, res) {
 
         //create variable to hold request parameters (specifically note's id)
         var noteId = req.params.id;
 
+        //read db JSON file and parse
         fs.readFile('../db/db.json', "utf8", function (err, res) {
             if (err) throw err;
             var everyNote = JSON.parse(res);
+            //use filter to create a new array that includes all notes but the one with the matching ID
             var newEveryNote = everyNote.filter(note => note.id != noteId);
             fs.writeFile("./db/db.json", JSON.stringify(newEveryNote, null, 2), err => {
                 if (err) throw err;
+                //send/show user the updated note data
                 res.send(noteData);
                 console.log("deleted note")
             });
-
-            //loop over all elements in the dbjson array and see if any id's match with note(id) that user wants to remove
-            // for (var i = 0; i < noteData.length; i++) {
-            //     if (deleteNote === noteData[i].id) {
-            //         // return res.json(noteData[i]); need splice() logic here
-            //     }
-            // }
 
             // res.send(noteData);
         });
     });
 
 }
-
-//  console.log(idIndex);
-//  console.log(noteData);
-// noteData.id = Math.floor(Math.random() * (noteData.length) + 1);
-// console.log(noteData.hasOwnProperty(id));
